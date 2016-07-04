f17f466d2b465545fe514dc050fe9c9c893c08dd
Alexander Mendez <bluespace@gmail.com>
2016-07-04 10:44:40 -0700

    update new relic

---
+++ newrelic.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['insighthubot'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '018dcb98ad239baf0decde983872ede378bfff0c',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
